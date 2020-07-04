6d963d1dbd5258a92dc0a8c02fbdb8ba59c4f232
seonyong-kim <introfor.blue@gmail.com>
2020-07-04 23:59:22 +0900

    solved Arithmetic Operators

---
+++ Arithmetic Operators.py
@@ -0,0 +1,14 @@
+'''
+Problem 7
+
+Arithmetic Operators
+
+What is print?
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
