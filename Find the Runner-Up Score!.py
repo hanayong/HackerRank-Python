2668753dd479e1f084f61ab3e63d49286e688b3f
hanayong <introfor.blue@gmail.com>
2020-07-12 12:37:29 +0900

    added comment

--- Find the Runner-Up Score!.py
+++ Find the Runner-Up Score!.py
@@ -2,6 +2,8 @@
 Find the Runner-Up Score!
 
 '''
-n = int(input())
-arr = set(map(int, input().split()))
-print(list(arr)[-2])
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(set(map(int, input().split())))
+    arr.sort()
+    print(arr[-2])
